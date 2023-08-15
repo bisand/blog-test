40d9898ca2d6a355f23b8a9a1203623d819f6b8c
André Biseth <andre@biseth.net>
2023-08-15 04:59:12 +0000

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@hebilicious/authjs-nuxt"],
+  // Optional default config
+  //  authJs: {
+  //    verifyClientOnEveryRequest: true,
+  //    guestRedirectTo: "/", // where to redirect if the user is authenticated
+  //    authenticatedRedirectTo: "/", // where to redirect if the user is not authenticated
+  //    baseUrl: ""
+  //  },
+  runtimeConfig: {
+    authJs: {
+      secret: process.env.NUXT_NEXTAUTH_SECRET // You can generate one with `openssl rand -base64 32`
+    },
+    github: {
+      clientId: process.env.NUXT_GITHUB_CLIENT_ID,
+      clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET
+    },
+    public: {
+      authJs: {
+        baseUrl: process.env.NUXT_NEXTAUTH_URL, // The base URL is used for the Origin Check in prod only
+        verifyClientOnEveryRequest: true // whether to hit the /auth/session endpoint on every client request
+      }
+    }
+  }
+})
