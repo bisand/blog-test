dfcff47f9186c2936525a68b55dd7475d532db97
André Syvertsen Biseth <andre@biseth.net>
2023-08-16 08:50:16 +0000

    Changed config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,11 +10,11 @@ export default defineNuxtConfig({
   //    baseUrl: ""
   //  },
   nitro: {
-    preset: "cloudflare"
+    preset: "cloudflare-pages"
   },
   runtimeConfig: {
     authJs: {
-      secret: process.env.NUXT_NEXTAUTH_SECRET // You can generate one with `openssl rand -base64 32`
+      secret: process.env.NUXT_SECRET // You can generate one with `openssl rand -base64 32`
     },
     github: {
       clientId: process.env.NUXT_GITHUB_CLIENT_ID,
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
     },
     public: {
       authJs: {
-        baseUrl: process.env.NUXT_NEXTAUTH_URL, // The base URL is used for the Origin Check in prod only
+        baseUrl: process.env.ORIGIN, // The base URL is used for the Origin Check in prod only
         verifyClientOnEveryRequest: true // whether to hit the /auth/session endpoint on every client request
       }
     }
--- test.js
+++
@@ -1,5 +0,0 @@
-const L = "undefined" != typeof globalThis ? globalThis : "undefined" != typeof self ? self : "undefined" != typeof global ? global : {};
-L.process = L.process || x;
-const R = L.process;
-const [Cs, Es] = R.versions.node.split(".").map((e2) => parseInt(e2, 10));
-console.log(Cs, Es);
